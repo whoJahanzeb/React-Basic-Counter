622860f8ece3a86590f3d13b9841d1cef97f72d8
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-01-15 22:30:47 +0500

    Modication in files and also install tailwind css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <>
-      <h1>This is first h1 tag in react JS.</h1>
+      <h1 className="text-center underline text-red-400">
+        This is first h1 tag in react JS.
+      </h1>
     </>
   );
 }
