4f5292c8f6b0e663373d437b05d91d2a051578f7
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-01-16 22:07:00 +0500

    Make basic counter in react js.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 function App() {
+  let [counter, setCounter] = useState(0);
+  function addValue() {
+    if (counter >= 20) {
+      return;
+    }
+    setCounter(counter + 1);
+  }
+  function removeValue() {
+    if (counter <= 0) {
+      return;
+    }
+    setCounter(counter - 1);
+  }
   return (
     <>
-      <h1 className="underline text-red-400">
-        This is first h1 tag in react JS.
-      </h1>
+      <div className="max-w-xl mx-auto space-y-4 text-center">
+        <h1 className="text-xl font-semibold">Value: {counter}</h1>
+        <button className="bg-green-500 text-white" onClick={addValue}>
+          Add Value
+        </button>
+        <br />
+        <button className="bg-red-500 text-white" onClick={removeValue}>
+          Remove Value
+        </button>
+      </div>
     </>
   );
 }
