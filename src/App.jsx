ee39af35ab9d18c011ee94b7fa17b13ac3097437
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2024-01-16 20:01:40 +0500

    Modication in files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-center underline text-red-400">
+      <h1 className="underline text-red-400">
         This is first h1 tag in react JS.
       </h1>
     </>
